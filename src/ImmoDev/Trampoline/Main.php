1367428ba41987cf7416e8ff8ec0165299bb7f99
Unreall <anggafiyan@gmail.com>
2024-05-10 23:16:55 +0700

    Update Main.php

--- src/ImmoDev/Trampoline/Main.php
+++ src/ImmoDev/Trampoline/Main.php
@@ -4,16 +4,25 @@ namespace ImmoDev\Trampoline;
 
 use pocketmine\event\Listener;
 use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
 use pocketmine\event\PlayerMoveEvent;
+use pocketmine\world\particle\HappyVillagerParticle;
 use pocketmine\math\Vector3;
 use pocketmine\block\BlockTypeIds;
 use pocketmine\event\entity\EntityDamageEvent;
 
 class Main extends PluginBase implements Listener {
 
+	public $config;
+
 	public function onEnable(): void {
-		$this->getLogger()->info("Plugin actived");
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		$this->saveDefaultConfig();
+		$this->getLogger()->info("Plugin Actived");
+	}
+
+	public function onDisable(): void {
+		$this->getLogger()->info("Plugin DeActived");
 	}
 
 	public function onMove(PlayerMoveEvent $event): void {
@@ -21,14 +30,14 @@ class Main extends PluginBase implements Listener {
 		$pos = $player->getPosition()->subtract(0, 1, 0);
         $block = $player->getWorld()->getBlock($pos);
         if ($block->getTypeId() == BlockTypeIds::SLIME) {
-            $motion = new Vector3(0, 1, 0);
+			$power = $this->getConfig()->get('trampoline_power', 0.8);
+            $motion = new Vector3(0, $power, 0);
 			$player->setMotion($motion);
+
+			$particle = new HappyVillagerParticle();
+			$player->getWorld()->addParticle($player->getPosition(), $particle);
         }
 	}
-
-	public function onDisable(): void {
-        $this->getLogger()->info("Plugin deactived");
-    }
 
 	public function onDamage(EntityDamageEvent $event): void {
 		if ($event->getcause () === EntityDamageEvent::CAUSE_FALL) {
